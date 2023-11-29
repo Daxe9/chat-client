3e806948b9a0934676e716ba14e16d13b8cc6207
lai2-24 <lai2-24@itismeucci.com>
2023-11-29 12:17:45 +0100

    first implementation

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,41 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        try {
+            Socket connection = new Socket("127.0.0.1", 42069);
+            SharedResource sr = new SharedResource();
+            ClientHandler ch = new ClientHandler(connection, sr);
+            ch.start();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            while (true) {
+                String serverResponse = in.readLine();
+                System.out.println("[LOG]: " + serverResponse);
+                sr.setMessage(serverResponse);
+                // TODO: handler upcoming message
+            }
+
+            /*
+             * sempre caro mi fu questo ermo colle
+             * e questa siepe che,
+             * da tanta parte dell'ultimo orizzontre il guardo esclude. ma sedendo e mirando
+             * interminati spazi e sovrumani silenzi e profondissima quiete io nel pensier
+             * mi fuggo ove per poco il cor non si spaura
+             */
+
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+
+        }
     }
 }
--- src/main/java/com/example/ClientHandler.java
+++ src/main/java/com/example/ClientHandler.java
@@ -1,16 +1,87 @@
 package com.example;
 
 import java.net.Socket;
+import java.util.Scanner;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
-public class ClientHandler {
+public class ClientHandler extends Thread {
     private Socket connection;
     private DataOutputStream out;
+    private SharedResource sr;
+    private Scanner scan;
+    private String nickname;
 
-    public ClientHandler(Socket connection) throws IOException {
+    public ClientHandler(Socket connection, SharedResource sr) throws IOException {
         this.connection = connection;
+        this.sr = sr;
+        this.scan = new Scanner(System.in);
         out = new DataOutputStream(connection.getOutputStream());
     }
 
+    private void nicknameHandler() throws IOException {
+        String msg = sr.getMessage();
+        if (msg.equals("name")) {
+            System.out.println("Inserisci il nickname: ");
+            String nickname = scan.nextLine();
+            this.nickname = nickname;
+            out.writeBytes("nickname," + nickname + "\n");
+        }
+    }
+
+    private void rules() {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+
+    /*
+     * @all ciao a tutti
+     * 
+     * @pippo ciao pippo
+     */
+    /*
+     * TODO:
+     * controllo su un nickname gia' esistente
+     * comunicare p2p
+     * comunicare in broadcast
+     * 
+     */
+    public void run() {
+        try {
+            Thread.sleep(1000);
+            nicknameHandler();
+            System.out.println("Benvenutə, " + nickname + "!");
+            // rules();
+
+            while (true) {
+                String userInput = scan.nextLine();
+                String[] userInputSplit = userInput.split(" ");
+                String cmd = userInputSplit[0].trim();
+                String originalMessage = "";
+                for (int i = 1; i < userInputSplit.length; i++) {
+                    originalMessage += userInputSplit[i] + " ";
+                }
+
+                switch (cmd) {
+                    case "@all":
+                        out.writeBytes("all," + originalMessage + "\n");
+                        break;
+
+                    case "@quit":
+
+                        break;
+                    default:
+                        String targetNickname = cmd.substring(1);
+                        out.writeBytes(targetNickname + "," + originalMessage + "\n"); 
+                        break;
+                }
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+    }
 }
