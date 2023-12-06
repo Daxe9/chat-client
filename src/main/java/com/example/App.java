7870bb5efa2e6fdf4bc138661348d681198ab48b
lai2-24 <lai2-24@itismeucci.com>
2023-12-06 13:38:23 +0100

    fix: l'infinito di leopardi

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -40,11 +40,23 @@ public class App {
             }
 
             /*
-             * sempre caro mi fu questo ermo colle
-             * e questa siepe che,
-             * da tanta parte dell'ultimo orizzontre il guardo esclude. ma sedendo e mirando
-             * interminati spazi e sovrumani silenzi e profondissima quiete io nel pensier
-             * mi fingo ove per poco il cor non si spaura
+             * L'infinito - Giacomo Brussi
+             * 
+             * Sempre caro mi fu questo ermo colle,
+             * e questa siepe, che da tanta parte
+             * dell'ultimo orizzonte il guardo esclude.
+             * Ma sedendo e mirando, interminati
+             * spazi di là da quella, e sovrumani 
+             * silenzi, e profonddissima quiete
+             * io nel pesneir mi fingo; ove per poho
+             * il cor non si spaura. E come il vento 
+             * odo stormir tra queste piante, io quello
+             * infinito silenzio a questa voce
+             * vo comparando: e mi sovvien l'eterno,
+             * e le morte stagioni, e la presente 
+             * e viva, e il suon di lei. Così tra questa 
+             * immensità s'annega il pensier mio:
+             * e il naufragar m'è dolce in questo mare. 
              */
 
         } catch (Exception e) {
